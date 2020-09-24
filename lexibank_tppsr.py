ccf1e5f9c68aca63bf5cd0d95447e2803819b72a
xrotwang <xrotwang@googlemail.com>
2020-09-24 08:51:47 +0200

    re-created CLDF data with updated scan URLs

--- lexibank_tppsr.py
+++ lexibank_tppsr.py
@@ -1,11 +1,11 @@
-from collections import defaultdict
+import re
+import collections
 
 import attr
 from pathlib import Path
 from pylexibank import Concept, Language, FormSpec, Lexeme
 from pylexibank.dataset import Dataset as BaseDataset
 from pylexibank import progressbar
-
 from csvw.metadata import URITemplate
 
 from lingpy import prosodic_string
@@ -38,6 +38,7 @@ class CustomLanguage(Language):
 @attr.s
 class CustomLexeme(Lexeme):
     Scan = attr.ib(default=None)
+    Objid = attr.ib(default=None)
     ProsodicStructure = attr.ib(default=None)
     SegmentedValue = attr.ib(default=None)
 
@@ -55,10 +56,7 @@ class Dataset(BaseDataset):
 
         # We can link forms to scans of the page in the source where they appear:
         args.writer.cldf["FormTable", "Scan"].valueUrl = URITemplate(
-            'https://ia801505.us.archive.org/BookReader/BookReaderImages.php'
-            '?zip=/28/items/gauchat-et-al-1925-tppsr/gauchat-et-al-1925-tppsr_jp2.zip'
-            '&file=gauchat-et-al-1925-tppsr_jp2/gauchat-et-al-1925-tppsr_{Scan}.jp2'
-            '&id=Z2F1Y2hhdC1ldC1hbC0xOTI1LXRwcHNy&scale=5')
+            'https://cdstar.shh.mpg.de/bitstreams/{Objid}/gauchat_et_al_1925_tppsr_{Scan}.png')
         for c in ['Population', 'SpeakerAge']:
             args.writer.cldf['LanguageTable', c].datatype.base = 'integer'
             args.writer.cldf['LanguageTable', c].datatype.minimum = 0
@@ -86,13 +84,26 @@ class Dataset(BaseDataset):
             
         languages = args.writer.add_languages(lookup_factory='Number')
 
-        phrase_data = defaultdict(dict)
+        def scan_number(bitstreams):
+            p = re.compile(r'tppsr_(?P<number>[0-9]{4})\.png')
+            for bs in bitstreams:
+                m = p.search(bs['bitstreamid'])
+                if m:
+                    return m.group('number')
+
+        scans = {
+            scan_number(o['bitstreams']): objid
+            for objid, o in self.raw_dir.read_json('tppsr_scans.json').items()}
+
+        phrase_data = collections.defaultdict(dict)
         for row1, row2 in progressbar(zip(values, forms), desc='cldfify'):
             entry = row1[2]
             for s, t in [('\u0320', '')]:
                 entry = entry.replace(s, t)
             tokens = self.tokenizer({}, entry.strip(), column='IPA')
-            page = int(concepts[row1[0]][1]) + (1 if int(row1[1]) > 31 else 0)
+            # Compute scan number from concept number and language number.
+            page = int(concepts[row1[0]][1]) + int(int(row1[1]) > 31)
+            scan = str(page + 18).rjust(4, '0')
 
             if row1[2].replace('_', '').replace('-', '').strip():
                 phrase_data[row1[1]][row1[0]] = (row2[2], row1[2])
@@ -104,7 +115,8 @@ class Dataset(BaseDataset):
                     Source=['Gauchat1925[{0}]'.format(page)],
                     Language_ID=languages[row1[1]],
                     Parameter_ID=concepts[row1[0]][0],
-                    Scan=str(page+18).rjust(4, '0'),
+                    Scan=scan,
+                    Objid=scans[scan],
                     ProsodicStructure=prosodic_string(tokens, _output='CcV'),
                     SegmentedValue=' '.join(self.tokenizer({}, entry, column='Graphemes'))
                 )
